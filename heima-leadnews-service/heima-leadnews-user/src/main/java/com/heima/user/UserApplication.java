cd0a4900b032b20822f579c1c66550a851307284
cjy16888 <1458545629@qq.com>
2023-05-07 16:30:29 +0800

    初始项目的基本工程的搭建

---
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
@@ -0,0 +1,17 @@
+package com.heima.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient //开启eureka/nacos客户端
+@MapperScan("com.heima.user.mapper")
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
