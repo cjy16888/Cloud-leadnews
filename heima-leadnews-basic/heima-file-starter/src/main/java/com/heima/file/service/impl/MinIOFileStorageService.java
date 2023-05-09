e2d004c235e1713169e9997ac730d369f1511a99
cjy16888 <1458545629@qq.com>
2023-05-09 18:43:26 +0800

    freemarker模板引擎的使用，MinIO分布式文件系统的存储服务的测试

---
+++ heima-leadnews-basic/heima-file-starter/src/main/java/com/heima/file/service/impl/MinIOFileStorageService.java
@@ -0,0 +1,175 @@
+package com.heima.file.service.impl;
+
+
+import com.heima.file.config.MinIOConfig;
+import com.heima.file.config.MinIOConfigProperties;
+import com.heima.file.service.FileStorageService;
+import io.minio.GetObjectArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.util.StringUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Slf4j
+@EnableConfigurationProperties(MinIOConfigProperties.class)
+@Import(MinIOConfig.class)
+public class MinIOFileStorageService implements FileStorageService {
+
+    @Autowired
+    private MinioClient minioClient;
+
+    @Autowired
+    private MinIOConfigProperties minIOConfigProperties;
+
+    private final static String separator = "/";
+
+    /**
+     * 上传文件的文件目录的命名，根据年月日进行层级的划分
+     * @param dirPath
+     * @param filename  yyyy/mm/dd/file.jpg
+     * @return
+     */
+    public String builderFilePath(String dirPath,String filename) {
+        StringBuilder stringBuilder = new StringBuilder(50);
+        if(!StringUtils.isEmpty(dirPath)){
+            stringBuilder.append(dirPath).append(separator);
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        String todayStr = sdf.format(new Date());
+        stringBuilder.append(todayStr).append(separator);
+        stringBuilder.append(filename);
+        return stringBuilder.toString();
+    }
+
+    /**
+     *  上传图片文件
+     * @param prefix  文件前缀
+     * @param filename  文件名
+     * @param inputStream 文件流 上传文件的内容
+     * @return  文件全路径
+     */
+    @Override
+    public String uploadImgFile(String prefix, String filename,InputStream inputStream) {
+        //创建文件的路径
+        String filePath = builderFilePath(prefix, filename);
+        try {
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+                    //MinIO服务器创建文件的路径
+                    .object(filePath)
+                    .contentType("image/jpg")
+                    //文件的内容，bucket 桶名称，需要和minio服务器上的桶名称一致
+                    .bucket(minIOConfigProperties.getBucket()).stream(inputStream,inputStream.available(),-1)
+                    .build();
+            //上传文件
+            minioClient.putObject(putObjectArgs);
+            //拼接文件的全路径，返回给前端，前端可以直接访问
+            StringBuilder urlPath = new StringBuilder(minIOConfigProperties.getReadPath());
+            urlPath.append(separator+minIOConfigProperties.getBucket());
+            urlPath.append(separator);
+            urlPath.append(filePath);
+            return urlPath.toString();
+        }catch (Exception ex){
+            log.error("minio put file error.",ex);
+            throw new RuntimeException("上传文件失败");
+        }
+    }
+
+    /**
+     *  上传html文件
+     * @param prefix  文件前缀
+     * @param filename   文件名
+     * @param inputStream  文件流
+     * @return  文件全路径
+     */
+    @Override
+    public String uploadHtmlFile(String prefix, String filename,InputStream inputStream) {
+        String filePath = builderFilePath(prefix, filename);
+        try {
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+                    .object(filePath)
+                    .contentType("text/html")
+                    .bucket(minIOConfigProperties.getBucket()).stream(inputStream,inputStream.available(),-1)
+                    .build();
+            minioClient.putObject(putObjectArgs);
+            StringBuilder urlPath = new StringBuilder(minIOConfigProperties.getReadPath());
+            urlPath.append(separator+minIOConfigProperties.getBucket());
+            urlPath.append(separator);
+            urlPath.append(filePath);
+            return urlPath.toString();
+        }catch (Exception ex){
+            log.error("minio put file error.",ex);
+            ex.printStackTrace();
+            throw new RuntimeException("上传文件失败");
+        }
+    }
+
+    /**
+     * 删除文件
+     * @param pathUrl  文件全路径
+     */
+    @Override
+    public void delete(String pathUrl) {
+        String key = pathUrl.replace(minIOConfigProperties.getEndpoint()+"/","");
+        int index = key.indexOf(separator);
+        String bucket = key.substring(0,index);
+        String filePath = key.substring(index+1);
+        // 删除Objects
+        RemoveObjectArgs removeObjectArgs = RemoveObjectArgs.builder().bucket(bucket).object(filePath).build();
+        try {
+            minioClient.removeObject(removeObjectArgs);
+        } catch (Exception e) {
+            log.error("minio remove file error.  pathUrl:{}",pathUrl);
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 下载文件
+     * @param pathUrl  文件全路径
+     * @return  文件流
+     *
+     */
+    @Override
+    public byte[] downLoadFile(String pathUrl)  {
+        String key = pathUrl.replace(minIOConfigProperties.getEndpoint()+"/","");
+        int index = key.indexOf(separator);
+        String bucket = key.substring(0,index);
+        String filePath = key.substring(index+1);
+        InputStream inputStream = null;
+        try {
+            // 获取Object Stat信息 (判断文件是否存在) 不存在会抛出异常 NoSuchKeyException
+            inputStream = minioClient.getObject(GetObjectArgs.builder().bucket(minIOConfigProperties.getBucket()).object(filePath).build());
+        } catch (Exception e) {
+            log.error("minio down file error.  pathUrl:{}",pathUrl);
+            e.printStackTrace();
+        }
+
+        //将文件流转换为byte数组
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] buff = new byte[100];
+        int rc = 0;
+        //读取文件流
+        while (true) {
+            try {
+                if (!((rc = inputStream.read(buff, 0, 100)) > 0)) {
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            byteArrayOutputStream.write(buff, 0, rc);
+        }
+        return byteArrayOutputStream.toByteArray();
+    }
+}
---
+++ heima-leadnews-service/heima-leadnews-articcle/src/test/java/article/test/ArticleFreemarkerTest.java
@@ -0,0 +1,75 @@
+package article.test;
+
+
+import com.alibaba.fastjson.JSONArray;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.heima.article.ArticleApplication;
+import com.heima.article.mapper.ApArticleContentMapper;
+import com.heima.article.mapper.ApArticleMapper;
+import com.heima.file.service.FileStorageService;
+import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.article.pojos.ApArticleContent;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest(classes = ArticleApplication.class)
+@RunWith(SpringRunner.class)
+public class ArticleFreemarkerTest {
+
+    @Autowired
+    private Configuration configuration;
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+
+    @Autowired
+    private ApArticleMapper apArticleMapper;
+
+    @Autowired
+    private ApArticleContentMapper apArticleContentMapper;
+
+    @Test
+    public void createStaticUrlTest() throws Exception {
+        //1.获取文章内容
+        ApArticleContent apArticleContent = apArticleContentMapper.selectOne(Wrappers.<ApArticleContent>lambdaQuery().eq(ApArticleContent::getArticleId, 1383827787629252610L));
+        if(apArticleContent != null && StringUtils.isNotBlank(apArticleContent.getContent())){
+            //2.文章内容通过freemarker生成html文件
+            StringWriter out = new StringWriter();
+            //获取模板
+            Template template = configuration.getTemplate("article.ftl");
+
+            //获取参数，返回给 ftl 模板
+            Map<String, Object> params = new HashMap<>();
+            //将文章内容转换成 JSONArray 对象，因为 String 对象在 ftl 模板中无法解析
+            //content 是 ftl 模板中的参数，不能随意修改
+            params.put("content", JSONArray.parseArray(apArticleContent.getContent()));
+
+            //将参数传入模板中，生成 html 文件
+            template.process(params, out);
+            InputStream is = new ByteArrayInputStream(out.toString().getBytes());
+
+            //3.把html文件上传到minio中
+            String path = fileStorageService.uploadHtmlFile("", apArticleContent.getArticleId() + ".html", is);
+
+            //4.修改ap_article表，保存static_url字段
+            ApArticle article = new ApArticle();
+            article.setId(apArticleContent.getArticleId());
+            //设置静态化地址，这个地址是 minio 中的地址，前端访问的时候，需要通过 nginx 转发到 minio 中
+            article.setStaticUrl(path);
+            apArticleMapper.updateById(article);
+        }
+    }
+}
